409ea52d711ed1b2d643fa30d6ddb344f1c0a69d
Elena Frey <ekfrey@stanford.edu>
2016-05-04 23:57:35 -0700

    dropdown menu and fix for image issue

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -5,6 +5,7 @@ import LessonPlanSidebarCell from './LessonPlanSidebarCell.jsx';
 import LessonsSidebarComponent from './LessonsSidebarComponent.jsx';
 import LessonPlanDetail from './LessonPlanDetail.jsx';
 import ReactFireMixin from 'reactfire';
+import {DropdownButton, MenuItem} from 'react-bootstrap';
 
 const App = React.createClass({
 
@@ -54,6 +55,7 @@ const App = React.createClass({
   		}
   	};
     var authData = this.firebaseRefs["lessonPlans"].getAuth();
+    let dropdownTitle = <span>Lucas T. <span className="glyphicon glyphicon-menu-down"></span></span>;
     if (authData) {
       console.log("User " + authData.uid + " is logged in with " + authData.provider);
       return  (
@@ -61,7 +63,11 @@ const App = React.createClass({
         <table className="topbar">
         <tbody>
           <tr>
-            <td width="300px"> <img id="current-child-picture" src="./public/BabyLucas.jpg" /><span id="current-child-name">Lucas T. <span className="glyphicon glyphicon-menu-down"></span></span><span id="logout-button" onClick={this.handleSignOut}>Logout</span></td>
+            <td width="300px"> <img id="current-child-picture" src="public/BabyLucas.JPG" />
+            <DropdownButton title={dropdownTitle} id="name-dropdown" noCaret={true}>
+              <MenuItem eventKey="1" onClick={this.handleSignOut}>Logout</MenuItem>
+            </DropdownButton>
+            </td>
             <td className="header-title"> Ravenswood Reads Lesson Planner </td>
             <td width="250px"><div className="create-button pull-right"> + </div></td>
           </tr>
